16b9386e012960f0a6ebb35cfd8e27fd56871b62
Andrea <llanesandrea0@gmail.com>
2024-12-19 16:26:16 -0300

    Cree eventos en productos

---
+++ js/script_productos.js
@@ -0,0 +1,89 @@
+const art001 = document.getElementById ("art_001");
+
+art001.addEventListener ("click", () => {
+    alert ("Remera Dama Flores, color blanco, 100% algodón, talles S al XXL.");
+});
+
+const art002 = document.getElementById ("art_002");
+
+art002.addEventListener ("click", () => {
+    alert ("Remera Dama Corazón, color negro, 100% algodón, talles 2 al 5.");
+});
+
+const art003 = document.getElementById ("art_003");
+
+art003.addEventListener ("click", () => {
+    alert ("Remera Dama Simbolo de Paz, color blanco, 100% algodón, todos los talles.");
+});
+
+const art004 = document.getElementById ("art_004");
+
+art004.addEventListener ("click", () => {
+    alert ("Remera Hombre cuello, color gris, 100% algodón, hasta talle XXL.");
+});
+
+const art005 = document.getElementById ("art_005");
+
+art005.addEventListener ("click", () => {
+    alert ("Remera Hombre cuello, color negro/celeste, 100% algodón, hasta talle 6.");
+});
+
+const art006 = document.getElementById ("art_006");
+
+art006.addEventListener ("click", () => {
+    alert ("Remera Hombre clásica, 4 colores, 100% algodón, hasta talle XXL.");
+});
+
+const art007 = document.getElementById ("art_007");
+
+art007.addEventListener ("click", () => {
+    alert ("Remera niño/niña, todos los colores, 100% algodón, todos los talles.");
+});
+
+const art008 = document.getElementById ("art_008");
+
+art008.addEventListener ("click", () => {
+    alert ("Remera niña dibujos, 100% algodón, hasta 7 años.");
+});
+
+const art009 = document.getElementById ("art_009");
+
+art009.addEventListener ("click", () => {
+    alert ("Remera niño dibujos, 100% algodón, hasta 7 años.");
+});
+
+const art010 = document.getElementById ("art_010");
+
+art010.addEventListener ("click", () => {
+    alert ("Vestido dama casual corto, 100% algodón, talles 1 al 3.");
+});
+
+const art011 = document.getElementById ("art_011");
+
+art011.addEventListener ("click", () => {
+    alert ("Vestido dama largo, 100% algodón, talles S al XXL.");
+});
+
+const art012 = document.getElementById ("art_012");
+
+art012.addEventListener ("click", () => {
+    alert ("Vestido dama cocteil, todos los talles.");
+});
+
+const art013 = document.getElementById ("art_013");
+
+art013.addEventListener ("click", () => {
+    alert ("Camisa y pantalón, Hombre, talles 34 al 50.");
+});
+
+const art014 = document.getElementById ("art_014");
+
+art014.addEventListener ("click", () => {
+    alert ("Traje formal, Hombre, todos los talles.");
+});
+
+const art015 = document.getElementById ("art_015");
+
+art015.addEventListener ("click", () => {
+    alert ("Traje semi formal, Hombre, todos los talles.");
+});
