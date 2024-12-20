b38fe1563bbb1a7b62127e5e3cf7acea330c169f
Andrea <llanesandrea0@gmail.com>
2024-12-20 10:16:55 -0300

    Muestra en la consola la lista de productos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 i = 1;
 
+while (i<=15){
+    if (i<=3){
+        console.log  ("Artículo", i, "Remera Dama");
+    } else  {
+        if (i <=6){
+            console.log ("Artículo",i, "Remera Hombre");
+        } else {
+            if (i <= 9) {
+                console.log ("Artículo", i, "Remera niños/as");
+            } else {
+                if (i <= 12) {
+                    console.log ("Artículo", i, "Vestidos Dama");
+                } else {
+                    console.log ("Artículo", i, "Traje Hombre");
+                }
+            }
+        }
+    }
+    i = i + 1;
+}
+
+
+
 
